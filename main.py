b871b4fed8925b7c1b6eda315290d708cb505259
spleider <kikkocollu@hotmail.it>
2020-10-07 18:06:46 +0200

    Update

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import telepot
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from SPARQLWrapper import SPARQLWrapper
 
-
-bot = telepot.Bot("1165813160:AAHeOi4DHXvSnwVYwLea3wcq9TgsGOhPUsM")
+f = open('token.txt', "r")
+bot = telepot.Bot(f.read())
 '''
 q =  ('SELECT DISTINCT ?station, ?orbits WHERE { '
 '?station a <http://dbpedia.org/ontology/SpaceStation> . '
@@ -30,7 +30,7 @@ def on_chat_message(msg):
 
         if 'hi' or 'ciao' or 'hello' in txt:
             bot.sendMessage(chat_id, 'Hello {}! I\'m SongBot.'.format(name))
-            bot.sendMessage(chat_id, "We can discovery a lot of albums together.")
+            bot.sendMessage(chat_id, "We can discovery a lot of albums together, gaaang.")
             bot.sendMessage(chat_id, "Give me informations like genre, year, nationality and so on")
         else:
             bot.sendMessage(chat_id, 'Sorry {}, I don\'t understand '.format(name))
