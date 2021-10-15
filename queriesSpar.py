62378ca33be73fcfec810991b1fdd0be083945ef
Enrico Collu <kikkocollu@hotmail.it>
2021-10-15 12:26:08 +0200

    Update Sparql Module

--- queriesSpar.py
+++ queriesSpar.py
@@ -1,3 +1,4 @@
+
 import json
 
 from SPARQLWrapper import SPARQLWrapper, JSON
@@ -13,13 +14,12 @@ else:
     # Handle target environment that doesn't support HTTPS verification
     ssl._create_default_https_context = _create_unverified_https_context
 
-
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 
 
 # Method for retrieve a Dataframe with songs of the artist given in input
 # Query to DBpedia with SparqlQuery
-def get_dbpedia_results(artistname, year = None, bef = False, aft = False):
+def get_dbpedia_results(artistname, year=None, bef=False, aft=False):
     if year is None:
         sparql.setQuery("""select distinct
         ?song
@@ -33,27 +33,27 @@ def get_dbpedia_results(artistname, year = None, bef = False, aft = False):
         {
         ?song a dbo:Single .
         ?song rdfs:label ?song_label.
-        
+
         ?song dbo:album ?album_name.
         ?album_name rdfs:label ?album_name_l.
-        
+
         ?song dbo:artist ?artist.
         ?artist rdfs:label ?artist_l.
-        
+
         ?song dbo:genre ?genre.
         ?genre rdfs:label ?genre_l.
-        
+
         ?song dbo:recordLabel ?recordLabel.
         ?recordLabel rdfs:label ?recordLabel_l.
-        
+
         ?song dbo:releaseDate ?releaseDate.
         FILTER(langMatches(lang(?song_label),"en")).
         FILTER(langMatches(lang(?album_name_l),"en")).
         FILTER(langMatches(lang(?artist_l),"en")).
         FILTER(langMatches(lang(?genre_l),"en")).
         FILTER(langMatches(lang(?recordLabel_l),"en")).
-        """+
-        "BIND( '" + str(artistname)+"'"+""" AS ?name)
+        """ +
+                        "BIND( '" + str(artistname) + "'" + """ AS ?name)
         FILTER (strstarts(lcase(str(?artist_l)), lcase(str(?name))))
         }
         group by ?song ?song_label ?album_name_l ?artist_l ?releaseDate
@@ -72,26 +72,21 @@ def get_dbpedia_results(artistname, year = None, bef = False, aft = False):
         {
         ?song a dbo:Single .
         ?song rdfs:label ?song_label.
-
         ?song dbo:album ?album_name.
         ?album_name rdfs:label ?album_name_l.
-
         ?song dbo:artist ?artist.
         ?artist rdfs:label ?artist_l.
-
         ?song dbo:genre ?genre.
         ?genre rdfs:label ?genre_l.
-
         ?song dbo:recordLabel ?recordLabel.
         ?recordLabel rdfs:label ?recordLabel_l.
-
         ?song dbo:releaseDate ?releaseDate.
         FILTER(langMatches(lang(?song_label),"en")).
         FILTER(langMatches(lang(?album_name_l),"en")).
         FILTER(langMatches(lang(?artist_l),"en")).
         FILTER(langMatches(lang(?genre_l),"en")).
-        FILTER(langMatches(lang(?recordLabel_l),"en"))."""+
-        'FILTER(?releaseDate < "' +str(year)+ '-01-01"^^xsd:date) '+"""
+        FILTER(langMatches(lang(?recordLabel_l),"en")).""" +
+                        'FILTER(?releaseDate < "' + str(year) + '-01-01"^^xsd:date) ' + """
         """ +
                         "BIND( '" + str(artistname) + "'" + """ AS ?name)
         FILTER (strstarts(lcase(str(?artist_l)), lcase(str(?name))))
@@ -112,19 +107,14 @@ def get_dbpedia_results(artistname, year = None, bef = False, aft = False):
             {
             ?song a dbo:Single .
             ?song rdfs:label ?song_label.
-
             ?song dbo:album ?album_name.
             ?album_name rdfs:label ?album_name_l.
-
             ?song dbo:artist ?artist.
             ?artist rdfs:label ?artist_l.
-
             ?song dbo:genre ?genre.
             ?genre rdfs:label ?genre_l.
-
             ?song dbo:recordLabel ?recordLabel.
             ?recordLabel rdfs:label ?recordLabel_l.
-
             ?song dbo:releaseDate ?releaseDate.
             FILTER(langMatches(lang(?song_label),"en")).
             FILTER(langMatches(lang(?album_name_l),"en")).
@@ -152,33 +142,28 @@ def get_dbpedia_results(artistname, year = None, bef = False, aft = False):
                 {
                 ?song a dbo:Single .
                 ?song rdfs:label ?song_label.
-
                 ?song dbo:album ?album_name.
                 ?album_name rdfs:label ?album_name_l.
-
                 ?song dbo:artist ?artist.
                 ?artist rdfs:label ?artist_l.
-
                 ?song dbo:genre ?genre.
                 ?genre rdfs:label ?genre_l.
-
                 ?song dbo:recordLabel ?recordLabel.
                 ?recordLabel rdfs:label ?recordLabel_l.
-
                 ?song dbo:releaseDate ?releaseDate.
                 FILTER(langMatches(lang(?song_label),"en")).
                 FILTER(langMatches(lang(?album_name_l),"en")).
                 FILTER(langMatches(lang(?artist_l),"en")).
                 FILTER(langMatches(lang(?genre_l),"en")).
                 FILTER(langMatches(lang(?recordLabel_l),"en")).""" +
-                    'FILTER(?releaseDate > "' + str(year) + '-01-01"^^xsd:date && ?releaseDate < "' + str(year) + '-12-31"^^xsd:date ) ' + """
+                        'FILTER(?releaseDate > "' + str(year) + '-01-01"^^xsd:date && ?releaseDate < "' + str(
+            year) + '-12-31"^^xsd:date ) ' + """
                 """ +
-                    "BIND( '" + str(artistname) + "'" + """ AS ?name)
+                        "BIND( '" + str(artistname) + "'" + """ AS ?name)
                 FILTER (strstarts(lcase(str(?artist_l)), lcase(str(?name))))
                 }
                 group by ?song ?song_label ?album_name_l ?artist_l ?releaseDate
                 LIMIT 5""")
-
 
     sparql.setReturnFormat(JSON)
     query_response = sparql.query().convert()
@@ -188,8 +173,10 @@ def get_dbpedia_results(artistname, year = None, bef = False, aft = False):
     df_results = pd.DataFrame(columns=['song_title', 'album', 'year', 'artist', 'label'])
 
     for result in query_response['results']['bindings']:
-        series = pd.Series([result["song_label"]["value"], result["album_name_l"]["value"] , result["releaseDate"]["value"],
-                            result["artist_l"]["value"], result["recordLabel_l"]["value"]], index = ['song_title', 'album', 'year', 'artist', 'label'] )
+        series = pd.Series(
+            [result["song_label"]["value"], result["album_name_l"]["value"], result["releaseDate"]["value"],
+             result["artist_l"]["value"], result["recordLabel_l"]["value"]],
+            index=['song_title', 'album', 'year', 'artist', 'label'])
         df_results = df_results.append(series, ignore_index=True)
 
     return df_results
